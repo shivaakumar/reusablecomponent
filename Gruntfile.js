926cb53f3607e842fc5aef6e7fd680a408300ba0
shivakumar.thyagaraj <shivakumar.thyagaraj@honeywell.com>
2017-07-04 12:47:27 +0530

    modified minified file name

--- Gruntfile.js
+++ Gruntfile.js
@@ -226,7 +226,7 @@ module.exports = function (grunt) {
     filerev: {
       dist: {
         src: [
-          '<%= yeoman.dist %>/scripts/{,*/}*.js',
+          // '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
           '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
@@ -334,13 +334,13 @@ module.exports = function (grunt) {
         }]
       }
     },
-
+        
     ngtemplates: {
       dist: {
         options: {
           module: 'accessControlComponentApp',
           htmlmin: '<%= htmlmin.dist.options %>',
-          usemin: 'scripts/scripts.js'
+          usemin: 'scripts/access-control-component.js'
         },
         cwd: '<%= yeoman.app %>',
         src: 'views/{,*/}*.html',
